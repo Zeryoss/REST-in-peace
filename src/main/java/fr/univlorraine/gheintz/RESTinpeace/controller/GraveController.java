147b8edde3351e802b84855f3052bf72584ddda8
Gilles Heintz <heintz.gilles@gmail.com>
2019-12-07 14:12:13 +0100

    Reorder routes

--- src/main/java/fr/univlorraine/gheintz/RESTinpeace/controller/GraveController.java
+++ src/main/java/fr/univlorraine/gheintz/RESTinpeace/controller/GraveController.java
@@ -38,17 +38,6 @@ public class GraveController {
         return ResponseEntity.ok().body("New Grave has been saved with ID:" + grave.getId());
     }
 
-    /*---Get a grave by id---*/
-    @GetMapping("/grave/{id}")
-    public ResponseEntity<Grave> get(@PathVariable("id") long id) {
-        Optional<Grave> grave = graveRepository.findById(id);
-        if (grave.isPresent()) {
-            return ResponseEntity.ok().body(grave.get());
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
-    }
-
     /*---get all graves---*/
     @GetMapping("/grave")
     public ResponseEntity<Iterable<Grave>> list(@RequestParam(required = false) String search) {
@@ -56,26 +45,6 @@ public class GraveController {
             return ResponseEntity.ok().body(graveRepository.findAll());
         }
         return ResponseEntity.ok().body(graveRepository.search(search));
-    }
-
-    /*---Update a grave by id---*/
-    @PutMapping("/grave/{id}")
-    public ResponseEntity<?> update(@PathVariable("id") long id, @RequestBody Grave updatedGrave) {
-
-        if (!graveRepository.findById(id).isPresent()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
-        updatedGrave.setId(id);
-        graveRepository.save(updatedGrave);
-
-        return ResponseEntity.ok().body("Grave has been updated successfully.");
-    }
-
-    /*---Delete a grave by id---*/
-    @DeleteMapping("/grave/{id}")
-    public ResponseEntity<?> delete(@PathVariable("id") long id) {
-        graveRepository.deleteById(id);
-        return ResponseEntity.ok().body("Grave has been deleted successfully.");
     }
 
     @GetMapping(value = "/grave/export")
@@ -107,6 +76,37 @@ public class GraveController {
                 .ok()
                 .headers(headers)
                 .body(new InputStreamResource(in));
+    }
+
+    /*---Get a grave by id---*/
+    @GetMapping("/grave/{id}")
+    public ResponseEntity<Grave> get(@PathVariable("id") long id) {
+        Optional<Grave> grave = graveRepository.findById(id);
+        if (grave.isPresent()) {
+            return ResponseEntity.ok().body(grave.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    /*---Update a grave by id---*/
+    @PutMapping("/grave/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") long id, @RequestBody Grave updatedGrave) {
+
+        if (!graveRepository.findById(id).isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        updatedGrave.setId(id);
+        graveRepository.save(updatedGrave);
+
+        return ResponseEntity.ok().body("Grave has been updated successfully.");
+    }
+
+    /*---Delete a grave by id---*/
+    @DeleteMapping("/grave/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") long id) {
+        graveRepository.deleteById(id);
+        return ResponseEntity.ok().body("Grave has been deleted successfully.");
     }
 
     @ExceptionHandler({ Exception.class })
